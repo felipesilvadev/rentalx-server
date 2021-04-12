a7928f75cd7ed68d331695decbdc95275a850b32
Felipe Silva <felipe.salaberga@gmail.com>
2021-04-12 20:53:59 -0300

    feat: category use case
    Signed-off-by: Felipe Silva <felipe.salaberga@gmail.com>

---
+++ src/modules/cars/repositories/implementations/CategoriesRepository.ts
@@ -0,0 +1,47 @@
+import { Category } from '../../models/Category';
+import {
+  ICategoriesRepository,
+  ICreateCategoryDTO,
+} from '../ICategoriesRepository';
+
+class CategoriesRepository implements ICategoriesRepository {
+  private categories: Category[];
+
+  private static INSTANCE: CategoriesRepository;
+
+  private constructor() {
+    this.categories = [];
+  }
+
+  public static getInstance(): CategoriesRepository {
+    if (!CategoriesRepository.INSTANCE) {
+      CategoriesRepository.INSTANCE = new CategoriesRepository();
+    }
+
+    return CategoriesRepository.INSTANCE;
+  }
+
+  findByName(name: string): Category | undefined {
+    const category = this.categories.find(category => category.name === name);
+
+    return category;
+  }
+
+  list(): Category[] {
+    return this.categories;
+  }
+
+  create({ name, description }: ICreateCategoryDTO): void {
+    const category = new Category();
+
+    Object.assign(category, {
+      name,
+      description,
+      created_at: new Date(),
+    });
+
+    this.categories.push(category);
+  }
+}
+
+export { CategoriesRepository };
