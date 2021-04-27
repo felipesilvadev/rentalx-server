da14beac72169d62783fc3b101b9388adc3d711d
Felipe Silva <felipe.salaberga@gmail.com>
2021-04-27 17:57:37 -0300

    feat: user authentication

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,13 @@
 import 'reflect-metadata';
 
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
+import 'express-async-errors';
 import swagger from 'swagger-ui-express';
 
 import './database';
 import './shared/container';
+
+import { AppError } from './errors/AppError';
 import { routes } from './routes';
 import swaggerSetup from './swagger.json';
 
@@ -15,6 +18,21 @@ app.use(express.json());
 app.use('/api-docs', swagger.serve, swagger.setup(swaggerSetup));
 
 app.use(routes);
+
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({
+        message: err.message,
+      });
+    }
+
+    return response.status(500).json({
+      status: 'error',
+      message: `Internal server error - ${err.message}`,
+    });
+  },
+);
 
 const PORT = 3333;
 app.listen(PORT, () => console.log('🚀 Server started on port:', PORT));
